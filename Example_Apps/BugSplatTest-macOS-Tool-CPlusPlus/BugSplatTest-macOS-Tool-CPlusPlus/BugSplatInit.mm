eae9462b7dea6c7af90af4c28b5eb04a3ef73c44
David Ferrero <david.ferrero@zion.com>
2025-03-05 13:57:21 -0700

    final improvements to BugSplatTest-macOS-Tool-CPlusPlus, adding README.md to example.

--- Example_Apps/BugSplatTest-macOS-Tool-CPlusPlus/BugSplatTest-macOS-Tool-CPlusPlus/BugSplatInit.mm
+++ Example_Apps/BugSplatTest-macOS-Tool-CPlusPlus/BugSplatTest-macOS-Tool-CPlusPlus/BugSplatInit.mm
@@ -14,13 +14,13 @@
 @end
 
 
+// Call the C++ function that uses Objective-C
 int bugSplatInit(const char * bugSplatDatabase)
 {
     @autoreleasepool {
-        // Call the C++ function that uses Objective-C
-        NSString *databaseName = [NSString stringWithCString:bugSplatDatabase encoding:NSUTF8StringEncoding];
-        NSLog(@"bugSplatInit called with database name: %@", databaseName);
-
+/*
+        // BugSplat expects bundleIdentifier, bundleVersion and optionally bundleMarketingVersion to be set within the Info.plist
+        // Uncomment this section to show the values that BugSplat is seeing and ensure they are set
         NSBundle *bundle = [NSBundle mainBundle];
         NSLog(@"bugSplatInit mainBundle: %@", bundle == nil ? @"nil" : @"not nil");
         NSString *bundleIdentifier = [bundle bundleIdentifier];
@@ -29,13 +29,18 @@ int bugSplatInit(const char * bugSplatDatabase)
         NSLog(@"bundleVersion: %@", bundleVersion == nil ? @"nil" : bundleVersion);
         NSString *bundleMarketingVersion = [[bundle infoDictionary] objectForKey: @"CFBundleShortVersionString"];
         NSLog(@"bundleMarketingVersion: %@", bundleMarketingVersion == nil ? @"nil" : bundleMarketingVersion);
-
+*/
 
         // Initialize BugSplat
         MyBugSplatDelegate *delegate = [[MyBugSplatDelegate alloc] init];
+
+        // Set a BugSplatDelegate
         [[BugSplat shared] setDelegate:delegate];
+
+        // Command Line Tools do not have a GUI. setAutoSubmitCrashReport: YES
         [[BugSplat shared] setAutoSubmitCrashReport:YES];
 
+/*
         // Optionally, add some attributes to your crash reports.
         // Attributes are artibrary key/value pairs that are searchable in the BugSplat dashboard.
         [[BugSplat shared] setValue:@"Value of Plain Attribute" forAttribute:@"PlainAttribute"];
@@ -43,12 +48,18 @@ int bugSplatInit(const char * bugSplatDatabase)
         [[BugSplat shared] setValue:[NSString stringWithFormat:@"Launch Date <![CDATA[%@]]> Value", [NSDate date]] forAttribute:@"CDATAExample"];
         [[BugSplat shared] setValue:[NSString stringWithFormat:@"<!-- 'value is > or < before' --> %@", [NSDate date]] forAttribute:@"CommentExample"];
         [[BugSplat shared] setValue:@"This value will get XML escaping because of 'this' and & and < and >" forAttribute:@"EscapingExample"];
+*/
+        NSString *databaseName = [NSString stringWithCString:bugSplatDatabase encoding:NSUTF8StringEncoding];
+        NSLog(@"bugSplatInit called with database name: %@", databaseName);
+
+        // Set the BugSplatDatabase name before calling start
+        [[BugSplat shared] setBugSplatDatabase:databaseName];
 
         // Don't forget to call start after you've finished configuring BugSplat
-        [[BugSplat shared] setBugSplatDatabase:databaseName];
         [[BugSplat shared] start];
 
-        // Hockey SDK expects a NSApplicationDidFinishLaunchingNotification to be sent before it will process crash reports.
+        // BugSplat expects a NSApplicationDidFinishLaunchingNotification to be sent before it will process crash reports.
+        // In a command line tool without a normal app launch, send the notification manually. This must be sent after [[BugSplat shared] start]
         [[NSNotificationCenter defaultCenter] postNotificationName:NSApplicationDidFinishLaunchingNotification object:nil userInfo:[NSDictionary new]];
     }
 
